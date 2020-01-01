5c6a55712b771ec78acb3949c427805ec50dde68
nmendozavd <nmendozavd@gmail.com>
2019-12-31 19:46:50 -0800

    Removed Key

--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,27 +1,31 @@
 import YOUTUBE_API_KEY from '../config/youtube.example.js';
 
-var searchYouTube = (options, callback) => {
-  console.log('I got called');
-  // TODO
-  $.ajax({
-    type: 'GET',
-    url: 'https://www.googleapis.com/youtube/v3/search',
-    data: {
-      key: 'YOUTUBE_API_KEY',
-      q: options.query,
-      part: 'snippet',
-      maxResults: options.max,
-      type: 'video',
-      videoEmbeddable: true,
-    },
-    success: function(data) {
-      // callback(data);
-      console.log('Success');
-    },
-    error: function(response) {
-      console.log('Request Failed');
-    }
-  });
-};
+// var searchYouTube = (options, callback) => {
+//   console.log('I got called');
+//   // TODO
+//   $.ajax({
+//     type: 'GET',
+//     url: 'https://www.googleapis.com/youtube/v3/search',
+//     data: {
+//       key: YOUTUBE_API_KEY,
+//       q: options.query,
+//       part: 'snippet',
+//       maxResults: options.max,
+//       type: 'video',
+//       videoEmbeddable: true,
+//     },
+//     success: function(data) {
+//       // callback(data);
+//       console.log('Success');
+//     },
+//     error: function(response) {
+//       console.log('Request Failed');
+//     }
+//   });
+// };
+
+// var searchYouTube = (options, callback) => {
+//   $.get()
+// };
 
 export default searchYouTube;
